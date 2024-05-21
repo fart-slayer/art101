159c101a0347fe6de54b5ec7f73628c54d5a9c17
fart-slayer <166079109+fart-slayer@users.noreply.github.com>
2024-05-20 17:45:09 -0700

    Lab 9 Draft 1
    I believe this is finished B)

---
+++ lab11 copy/js/lab.js
@@ -0,0 +1,10 @@
+// index.js - Lab 7: Functions
+// Author: Alexandru Wechsler
+// Credits: Cynthia Ramirez, Chat GBT 🤍
+// Date: May 12, 2024
+
+// Define Variables + Objects
+
+// Functions
+
+// Output
--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,10 +1,32 @@
-// index.js - Lab 7: Functions
+// index.js - Lab 9: Libraries and jQuery
 // Author: Alexandru Wechsler
 // Credits: Cynthia Ramirez, Chat GBT 🤍
-// Date: May 12, 2024
+// Date: May 20, 2024
 
-// Define Variables + Objects
+//CHALLENGE
+//Using jQuery add buttons to HTML
+$("#challenge").append("<button id='button-challenge'>Make Pretty!</button>"); 
+// add a click listener to the challenge button
+$("#button-challenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
 
-// Functions
+//PROBLEMS
+//Using jQuery add buttons to HTML
+$("#problems").append("<button id='button-problems'>Make Pretty!</button>"); 
+// add a click listener to the challenge button
+$("#button-problems").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#problems").toggleClass("special");
+});
 
-// Output
+//RESULTS
+//Using jQuery add buttons to HTML
+$("#results").append("<button id='button-results'>Make Pretty!</button>"); 
+// add a click listener to the challenge button
+$("#button-results").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#results").toggleClass("special");
+});
+
