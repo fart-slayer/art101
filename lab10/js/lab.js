afa5504c114397ba9d071b17913ab75c9aea2ee2
fart-slayer <166079109+fart-slayer@users.noreply.github.com>
2024-05-22 15:33:48 -0700

    Lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,10 +1,24 @@
-// index.js - Lab 7: Functions
+// index.js - Lab 10: JavaScript for the Web
 // Author: Alexandru Wechsler
 // Credits: Cynthia Ramirez, Chat GBT 🤍
-// Date: May 12, 2024
+// Date: May 22, 2024
 
-// Define Variables + Objects
+function generateRandomText() {
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+  }
 
-// Functions
+// click listener for button
+$("#make-convo").click(function(){
+    // get new fake dialogue
+    const newText = generateRandomText();   
+    // append a new div to our output div
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+});
 
-// Output
