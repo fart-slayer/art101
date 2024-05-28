36d4e01f946c79ab4baef201f1c8021a7dcf2326
fart-slayer <166079109+fart-slayer@users.noreply.github.com>
2024-05-28 13:45:14 -0700

    Lab 11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,10 +1,21 @@
-// index.js - Lab 7: Functions
+// index.js - Lab 11: JavaScript Events and Forms
 // Author: Alexandru Wechsler
 // Credits: Cynthia Ramirez, Chat GBT 🤍
-// Date: May 12, 2024
+// Date: May 25, 2024
 
-// Define Variables + Objects
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+    // We have to convert our string to an array and back again to sort it
+    return inputString.split('').sort().join('');
+  }
 
-// Functions
+// click listener for button
+$("#submit").click(function() {
+    // get value of input field
+    const userName = $("#user-name").val();
+    // now let's sort it
+    userNameSorted = sortString(userName);
+    // append a new div to our output div
+    $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
 
-// Output
