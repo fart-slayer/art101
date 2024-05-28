9d4fa8b1bd0cf3559dcd166aa309e06223e8a0be
fart-slayer <166079109+fart-slayer@users.noreply.github.com>
2024-05-28 14:38:48 -0700

    Lab 12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,10 +1,29 @@
-// index.js - Lab 7: Functions
+// index.js - Lab 12: Conditionals
 // Author: Alexandru Wechsler
 // Credits: Cynthia Ramirez, Chat GBT 🤍
-// Date: May 12, 2024
+// Date: May 28, 2024
 
-// Define Variables + Objects
+function sortingHat(str) {
+    len = str.length;
+    mod = len % 4;
+    if (mod == 0) {
+       return "Gryffandor" 
+    }
+    else if (mod == 1) {
+        return "Ravenclaw"
+    }
+    else if (mod == 2) {
+        return "Slytherin"
+    }
+    else if (mod == 3) {
+        return "Hufflepuff"
+    }
+};
 
-// Functions
-
-// Output
+var myButton = document.getElementById("submit");
+myButton.addEventListener("click", function(){
+    var name = document.getElementById("input").value;
+    var house = sortingHat(name);
+    newText = "<p>The sorting hat has sorted you into " + house + "! </p>";
+    document.getElementById("output").innerHTML = newText;
+});
